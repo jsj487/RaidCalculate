d45961f1a7e917d43bd8cc32b884b3c96bc99ad4
jsj487 <jsj487@naver.com>
2025-05-28 11:24:02 +0900

    fix: 보석 깐부, 팔찌 시뮬 중단

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -90,21 +90,21 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           <NavLink to="/GoldCalc" $isActive={location.pathname === "/GoldCalc"}>
             주간 레이드 계산기
           </NavLink>
-          <NavLink
+          {/* <NavLink
             to="/JewelFriend"
             $isActive={location.pathname === "/JewelFriend"}
           >
             보석 깐부 찾기
-          </NavLink>
+          </NavLink> */}
           <NavLink to="/Package" $isActive={location.pathname === "/Package"}>
             패키지 계산기
           </NavLink>
           <NavLink to="/Auction" $isActive={location.pathname === "/Auction"}>
             경매 계산기
           </NavLink>
-          <NavLink to="/Bracelet" $isActive={location.pathname === "/Bracelet"}>
+          {/* <NavLink to="/Bracelet" $isActive={location.pathname === "/Bracelet"}>
             팔찌 시뮬레이터
-          </NavLink>
+          </NavLink> */}
         </Nav>
       </Header>
 
